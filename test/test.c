b21459b12225a8a89db4aa8da9f599f3343dcc3f
Greg Hairfield <ghairfield@gmail.com>
2019-11-08 19:16:09 -0800

    Reformatted

--- test/test.c
+++ test/test.c
@@ -2,13 +2,14 @@
 #include <stdio.h>
 
 
-int main()
+int
+main ()
 {
   char sen1[] = "This is the first test string of the public string class.";
-  String *s = str_create(sen1, strlen(sen1));
+  String *s = str_create (sen1, strlen (sen1));
 
-  fprintf (stdout, "We got back \"%s\"<S: %zu|C: %zu>\n", s->str, s->size, 
-           s->capacity);
+  fprintf (stdout, "We got back \"%s\"<S: %zu|C: %zu>\n", s->str, s->size,
+	   s->capacity);
   str_destroy (&s);
   return 0;
 }
