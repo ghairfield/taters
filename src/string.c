131fa4304c5f5b44f8287971bedf318415bf28d0
Greg Hairfield <ghairfield@gmail.com>
2019-11-08 18:20:10 -0800

    Indenting

--- src/string.c
+++ src/string.c
@@ -1,63 +1,76 @@
 #include "string.h"
 
-String *s_init()
+String *
+s_init ()
 {
-  String *s = malloc(sizeof(String));
-  if ( !s) {
-    /* Log error or something */
-    perror("Could not create the string, out of memory.\n");
-    return NULL;
-  }
+  String *s = malloc (sizeof (String));
+  if (!s)
+    {
+      /* Log error or something */
+      perror ("Could not create the string, out of memory.\n");
+      return NULL;
+    }
 
-  s->str      = NULL;
+  s->str = NULL;
   s->capacity = S_INIT_SIZE;
-  s->size     = 0;
+  s->size = 0;
   return s;
 }
 
-static int copy_string(String *s, const char *src, size_t sz)
+static int
+copy_string (String * s, const char *src, size_t sz)
 {
   size_t init_size = S_INIT_SIZE;
-  if (sz > init_size) {
-    /* Must create large string that is multiple of S_INIT_SIZE */
-  }
+  if (sz > init_size)
+    {
+      /* Must create large string that is multiple of S_INIT_SIZE */
+    }
 
-  s->str = malloc(sizeof(char) * init_size);
-  if ( !s->str) {
-    /* Log error or something */
-    return -1;
-  }
+  s->str = malloc (sizeof (char) * init_size);
+  if (!s->str)
+    {
+      /* Log error or something */
+      return -1;
+    }
 
-  memcpy(s->str, src, sz);
+  memcpy (s->str, src, sz);
   s->size = sz;
   return 0;
 }
 
-String *str_create(const char *src, size_t sz)
+String *
+str_create (const char *src, size_t sz)
 {
-  String* s = s_init(); 
-  if ( !s) return NULL; /* Error should of been logged. */
-  if (copy_string(s, src, sz)) return NULL;
+  String *s = s_init ();
+  if (!s)
+    return NULL;		/* Error should of been logged. */
+  if (copy_string (s, src, sz))
+    return NULL;
 
   return s;
 }
 
-void str_destroy(String **s)
+void
+str_destroy (String ** s)
 {
-  if (*s) {
-    if ((*s)->str) free((*s)->str);
-    free (*s);
-    *s = NULL;
-  }
+  if (*s)
+    {
+      if ((*s)->str)
+	free ((*s)->str);
+      free (*s);
+      *s = NULL;
+    }
 
   *s = NULL;
 }
 
-int main(/*int argc, char* argv[]*/)
+int
+main ( /* int argc, char* argv[] */ )
 {
   char test[] = "This is a test of the string thing.";
-  String *s = str_create(test, strlen(test));
-  fprintf(stdout, "We got back \"%s\"<S: %zu|C: %zu>\n", s->str, s->size, s->capacity);
-  str_destroy(&s);
+  String *s = str_create (test, strlen (test));
+  fprintf (stdout, "We got back \"%s\"<S: %zu|C: %zu>\n", s->str, s->size,
+	   s->capacity);
+  str_destroy (&s);
   return 0;
 }
--- src/string.h
+++ src/string.h
@@ -3,12 +3,12 @@
  * 30 Oct 2019
  */
 #include <stdlib.h>
-#include <string.h> 
+#include <string.h>
 #include <stdio.h>
 
 
-#define S_INIT_SIZE 256 /* Size of the initial string */
-#define S_GROW_SIZE 2   /* Growth multiplier          */
+#define S_INIT_SIZE 256		/* Size of the initial string */
+#define S_GROW_SIZE 2		/* Growth multiplier          */
 
 /*
  * Main string structure. The user is responsible for
@@ -16,9 +16,9 @@
  */
 typedef struct String
 {
-  size_t capacity; /* Capacity of str from 1 to N */
-  size_t size;     /* Size of the used string     */
-  char   *str;     /* sizeof capacity             */
+  size_t capacity;		/* Capacity of str from 1 to N */
+  size_t size;			/* sizeof str                  */
+  char *str;			/* sizeof capacity             */
 } String;
 
 
@@ -30,7 +30,7 @@ typedef struct String
  * Returns:   A pointer to a String structure
  * @see       void str_delete(String *s)
  */
-String *str_create(const char *src, size_t sz);
+String *str_create (const char *src, size_t sz);
 
 /*
  * str_destroy Deletes all the memory of the String parameter.
@@ -38,5 +38,4 @@ String *str_create(const char *src, size_t sz);
  * @s String to destroy
  * Returns: None
  */
-void str_destroy(String **s);
-
+void str_destroy (String ** s);
